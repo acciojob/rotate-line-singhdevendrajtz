6081e12b544de6c174aae0eaa7ce53da0293cb55
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-29 01:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const my_line=document.querySelector("#line")
+let interval=20;
+let initial_deg=0;
+setInterval(function (){
+//console.log(initial_deg)
+	initial_deg+=2;
+my_line.style.transform=`rotate(${initial_deg}deg)`
+
+},interval);
+
